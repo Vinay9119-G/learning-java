be141d5e1c79248aebe2a469b17c8ed206fc03f3
Vinay Ambawat <ambawatavinay@gmail.com>
2025-06-04 15:35:17 +0530

    learning Conditionals class

---
+++ src/com/company/Conditionals/if_else.java
@@ -0,0 +1,25 @@
+package com.company.Conditionals;
+
+public class if_else {
+    public static void main(String[] args) {
+//        int a = 10;
+//        if (a < 5){
+//            System.out.println("The number is greater then 5.");
+//        }
+//        else {
+//            System.out.println("System is not responding well.");
+//        }
+
+        int a = 15;
+        if (a < 10 ){
+            System.out.println("the number is less than 10.");
+        }
+        else if (a < 20 ){
+            System.out.println("the number is between 10 and 20.");
+        }else {
+            System.out.println("The number is 20 or more.");
+        }
+
+
+    }
+}
